c43fff7e3d75768492606042970daa98ef5255d0
Angold-4 <wangold4w@gmail.com>
2022-08-19 20:38:30 +0800

    Optimized the debug function of template

--- src/template.cpp
+++ src/template.cpp
@@ -12,31 +12,70 @@
 #include <numeric>
 #include <queue>
 #include <random>
+#include <sstream>
 #include <set>
 #include <vector>
+#include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
 using namespace std;
 
-#ifdef DEBUG
-  #define debug(args...)            {dbg, args; cerr<<endl;}
-#else
-  #define debug(args...)            // Just strip off all debug tokens
-#endif
-
-struct debugger
-{
-  template<typename T> debugger& operator , (const T& v)
-  {    
-    cerr << v << " ";    
-    return *this;    
-  }
-} dbg;
-
-void run_case() {
-
+// https://codeforces.com/blog/entry/76087
+template<class T1, class T2>
+std::ostream &operator<<(std::ostream &os, const std::pair<T1,T2> &p){
+  os << '{' << p.first << ", " << p.second << '}';
+  return os;
 }
 
+template<typename C, 
+         typename T = std::decay<decltype(*begin(std::declval<C>()))>,
+         typename std::enable_if<!std::is_same<C, std::string>::value>::type* = nullptr
+         >
+std::ostream &operator<<(std::ostream &os, const C &container){
+  bool first = true;
+  std::stringstream ss; 
+  ss << '[';
+  for(const auto &x : container){
+    if (!first){
+      ss << ", ";
+    }
+    first = false;
+    ss << x;
+  }
+  ss << ']';
+  return os << ss.str();
+}
+
+vector<string> vec_splitter(string s) {
+	s += ',';
+	vector<string> res;
+	while(!s.empty()) {
+		res.push_back(s.substr(0, s.find(',')));
+		s = s.substr(s.find(',') + 1);
+	}
+	return res;
+}
+
+void debug_out(
+vector<string> __attribute__ ((unused)) args,
+__attribute__ ((unused)) int idx, 
+__attribute__ ((unused)) int LINE_NUM) { cerr << endl; } 
+template <typename Head, typename... Tail>
+void debug_out(vector<string> args, int idx, int LINE_NUM, Head H, Tail... T) {
+	if(idx > 0) cerr << ", "; else cerr << "Line(" << LINE_NUM << ") ";
+	stringstream ss; ss << H;
+	cerr << args[idx] << " = " << ss.str();
+	debug_out(args, idx + 1, LINE_NUM, T...);
+}
+
+#ifdef DEBUG
+#define debug(...) debug_out(vec_splitter(#__VA_ARGS__), 0, __LINE__, __VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+void run_case() {
+}
 
 int main() {
   int tests;
@@ -45,4 +84,3 @@ int main() {
   while (tests-- > 0) 
     run_case();
 }
-
